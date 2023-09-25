35859e95a9a9056c4479cca07f1c2aa0b6597893
madstone290 <madstone290@gmail.com>
2023-09-25 14:00:38 +0900

    Feat draw border line

---
+++ js/data.js
@@ -0,0 +1,14 @@
+var listDatasource = [
+    { key: 1, value: "H34A2900001" },
+    { key: 2, value: "H34A2900002" },
+    { key: 3, value: "H34A2900003" },
+    { key: 4, value: "H34A2900004" },
+    { key: 5, value: "H34A2900005" },
+    { key: 6, value: "H34A2900006" },
+    { key: 7, value: "H34A2900007" },
+    { key: 8, value: "H34A2900008" },
+    { key: 9, value: "H34A2900009" },
+    { key: 10, value: "H34A2900010" },
+    { key: 11, value: "H34A2900011" },
+    { key: 12, value: "H34A2900012" },
+];
--- js/status-chart.js
+++ js/status-chart.js
@@ -1,18 +1,3 @@
-const listDatasource = [
-    { key: 1, value: "H34A2900001" },
-    { key: 2, value: "H34A2900002" },
-    { key: 3, value: "H34A2900003" },
-    { key: 4, value: "H34A2900004" },
-    { key: 5, value: "H34A2900005" },
-    { key: 6, value: "H34A2900006" },
-    { key: 7, value: "H34A2900007" },
-    { key: 8, value: "H34A2900008" },
-    { key: 9, value: "H34A2900009" },
-    { key: 10, value: "H34A2900010" },
-    { key: 11, value: "H34A2900011" },
-    { key: 12, value: "H34A2900012" },
-];
-
 const listBoxId = "ab-chart-list-box";
 const canvasId = "ab-chart-content-canvas";
 const canvasBoxId = "ab-chart-content-canvas-box";
@@ -45,7 +30,7 @@ function createList() {
         // grid line
         const line = document.createElement("div");
         line.classList.add("ab-chart-hline");
-        line.style.top = `${rowHeight * (i + 1)}px`;
+        line.style.top = `${(rowHeight + 1) * (i + 1) - 1}px`;
         line.style.width = canvas.scrollWidth;
         canvas.appendChild(line);
 
