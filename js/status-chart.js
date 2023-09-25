46dac832fd27bf0e493b65c7d41e2faf4df34af3
madstone290 <madstone290@gmail.com>
2023-09-25 14:52:50 +0900

    Feat draw timeline header

--- js/status-chart.js
+++ js/status-chart.js
@@ -12,6 +12,41 @@ const css = {
     }
 }
 
+const timeline = function () {
+    const timeline_title_id = "ab-chart-content-timeline-title";
+    const timline_header_box_id = "ab-chart-content-timeline-header-box";
+    const timeline_header_id = "ab-chart-content-timeline-header";
+    const timeline_status_id = "ab-chart-content-timeline-status";
+
+    function setTitle(name) {
+        const titleElement = document.getElementById(timeline_title_id);
+        titleElement.innerText = name;
+    }
+    function drawHeaders() {
+        const header = document.getElementById(timeline_header_id);
+        for (const data of timelineHeaders) {
+            const div = document.createElement("div");
+            div.innerText = data;
+            div.classList.add("ab-chart-content-timeline-header-item");
+            header.appendChild(div);
+        }
+
+        const canvasBox = document.getElementById(canvasBoxId);
+        const box = document.getElementById(timline_header_box_id);
+        canvasBox.addEventListener("scroll", (e) => {
+            console.log(e);
+            box.scrollLeft = canvasBox.scrollLeft;
+        });
+    }
+
+    return {
+        setTitle,
+        drawHeaders
+    }
+}();
+
+
+
 function createList() {
     const box = document.getElementById(listBoxId);
     const canvas = document.getElementById(canvasId);
@@ -30,7 +65,7 @@ function createList() {
         // grid line
         const line = document.createElement("div");
         line.classList.add("ab-chart-hline");
-        line.style.top = `${(rowHeight + 1) * (i + 1) - 1}px`;
+        line.style.top = `${rowHeight * (i + 1) - 1}px`;
         line.style.width = canvas.scrollWidth;
         canvas.appendChild(line);
 
@@ -40,12 +75,16 @@ function createList() {
     canvas.style.height = `${box.scrollHeight}px`;
 
     const canvasBox = document.getElementById(canvasBoxId);
-    canvasBox.onscroll = (e) => {
+    canvasBox.addEventListener("scroll", (e) => {
         console.log(e);
         box.scrollTop = canvasBox.scrollTop;
-    };
+    });
 }
 
 window.addEventListener("load", () => {
+
+    timeline.setTitle("Timeline");
+    timeline.drawHeaders();
+
     createList();
 });
